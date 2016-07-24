bb3d63c31bc9cb6869b4b2775868672ccfe6c6b1
MarkZhuYiQi <zhuyiqi_026@163.com>
2016-07-24 23:05:22 +0800

    learning

--- scan_explorer.js
+++ scan_explorer.js
@@ -53,6 +53,35 @@ alert(hasIEPlugin("ShockwaveFlash.ShockwaveFlash"));//检测flash
 */
 
 /*
+//兼容所有浏览器的检查控件
+function hasPlugin(name){
+    var name=name.toLowerCase();
+    for(var i=0;i<navigator.plugins.length;i++) {
+        if (navigator.plugins[i].name.toLowerCase().indexOf(name) > -1) {
+            return true;
+        }
+    }
+    return false;
+}
+function hasIEPlugin(name){
+    try{
+        new ActiveXObject(name);
+        return true;
+    }catch(e){
+        return false;
+    }
+}
+function hasFlash() {
+    var result = hasPlugin("Flash");
+    if (!result) {
+        result = hasIEPlugin("ShockwaveFlash.ShockwaveFlash");
+    }
+    return result;
+}
+alert(hasFlash());
+*/
+
+/*
 //遍历非IE下所有MIME类型信息
 for(var i=0;i<navigator.mimeTypes.length;i++){
     if(navigator.mimeTypes[i].enabledPlugin!=null){
@@ -78,3 +107,128 @@ if(typeof width!="number"){
 }
 */
 
+/*
+//能力检测
+var width=window.innerWidth;
+if(typeof width!="number"){
+    width=document.documentElement.clientWidth;
+}
+alert(width);
+*/
+
+/*
+//怪癖检测 bug
+var box={
+    toString:function(){},
+    toString2:function(){}
+};
+for(var o in box){
+    alert(o);
+}*/
+
+//用户代理检测
+document.write(navigator.userAgent);
+document.write("<br />");
+document.write(navigator.platform);
+var client=function(){
+    //引擎
+    var engine={
+        ie:false,       //用于确定是否是IE引擎
+        gecko:false,
+        webkit:false,
+        KHTML:false,
+        opera:false,
+        ver:0   //引擎版本
+    };
+
+    //浏览器
+    var browser={
+        firefox:false,
+        ie:false,
+        chrome:false,
+        safari:false,
+        opera:false,
+        ver:0,
+        name:""
+    };
+    //系统
+    var system={
+        windows:false,
+        mac:false,
+        unix:false,
+        systemname:""
+    };
+
+
+    //核心检测程序
+    var ua=navigator.userAgent;
+    var p=navigator.platform;
+    if(p.indexOf("Win")==0){
+        system.windows=true;
+        system.systemname="windows";
+    }
+
+
+
+    if(window.opera){
+        engine.opera=browser.opera=true;      //表示是opera浏览器
+        engine.ver=browser.ver=window.opera.version();
+        browser.name="Opera";
+    }
+    else if(/AppleWebKit\/(\S+)/.test(ua)){
+        engine.webkit=browser.chrome=true;
+        engine.ver=browser.ver=RegExp["$1"];
+        if(/Chrome\/(\S+)/.text(ua)){
+            browser.name="Chrome";
+            browser.ver=RegExp["$1"];
+            browser.chrome=true;
+        }else{
+            browser.name="safari";
+            if(/Version\/(\S+)/.test(ua)){
+                browser.ver=RegExp["$1"];
+            }
+            browser.safari=true;
+        }
+    }
+    else if(/rv:([^\)]+)\) Gecko\/\d{8}/.test(ua)){
+        engine.gecko=browser.firefox=true;
+        engine.ver=browser.ver=RegExp['$1'];
+        if(/FireFox\/(\S+)/.test(ua)){
+            browser.name="Firefox";
+            browser.firefox=true;
+            browser.ver=RegExp["$1"];
+        }
+
+    }
+    else if(/MSIE ([^;]+)/.test(ua)){
+        engine.ie=browser.ie=true;
+        engine.ver=browser.ver=RegExp["$1"];
+        browser.name="Internet Explorer";
+    }
+
+    //可以同时返回浏览器，系统和引擎
+    return {
+        engine:engine,  //属性：对象
+        browser:browser,
+        system:system
+
+    };
+}();
+// alert(client.engine.ie);
+// alert(client.engine.ver);
+
+//window对象支持opera属性
+if(client.engine.opera){
+    alert("目前使用的opera版本为"+client.engine.ver);
+}
+if(client.engine.webkit){
+    alert("webkit engine version is: "+client.engine.ver);
+}
+if(client.engine.gecko){
+    alert("gecko engine version is: "+client.engine.ver);
+}
+if(client.engine.ie){
+    alert("ie engine version is: "+client.engine.ver);
+}
+alert(client.browser.name);
+alert(client.system.systemname);
